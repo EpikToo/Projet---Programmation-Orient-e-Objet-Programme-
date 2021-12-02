5ab3b2c88555d6a756fb7c27ba03f3e7a0d77d47
Paul DESPONT <paul.despont@viacesi.fr>
2021-12-02 11:17:17 +0100

    modif vue ville

--- VueVille.h
+++ VueVille.h
@@ -68,6 +68,7 @@ namespace ProjetProgrammationOrientéeObjetProgramme {
 
 
 	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::Label^ label3;
 
 
 
@@ -104,43 +105,51 @@ namespace ProjetProgrammationOrientéeObjetProgramme {
 			this->btn_first = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dgv_enr))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// dgv_enr
 			// 
 			this->dgv_enr->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dgv_enr->Location = System::Drawing::Point(334, 85);
+			this->dgv_enr->Location = System::Drawing::Point(418, 106);
+			this->dgv_enr->Margin = System::Windows::Forms::Padding(4);
 			this->dgv_enr->Name = L"dgv_enr";
-			this->dgv_enr->Size = System::Drawing::Size(703, 450);
+			this->dgv_enr->RowHeadersWidth = 51;
+			this->dgv_enr->Size = System::Drawing::Size(879, 562);
 			this->dgv_enr->TabIndex = 0;
 			this->dgv_enr->CellContentClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &VueVille::dgv_enr_CellContentClick);
 			// 
 			// btn_load
 			// 
-			this->btn_load->Location = System::Drawing::Point(143, 353);
+			this->btn_load->BackColor = System::Drawing::SystemColors::ActiveCaption;
+			this->btn_load->Location = System::Drawing::Point(183, 441);
+			this->btn_load->Margin = System::Windows::Forms::Padding(4);
 			this->btn_load->Name = L"btn_load";
-			this->btn_load->Size = System::Drawing::Size(165, 237);
+			this->btn_load->Size = System::Drawing::Size(206, 191);
 			this->btn_load->TabIndex = 1;
-			this->btn_load->Text = L"Charger";
-			this->btn_load->UseVisualStyleBackColor = true;
+			this->btn_load->Text = L"Afficher";
+			this->btn_load->UseVisualStyleBackColor = false;
 			this->btn_load->Click += gcnew System::EventHandler(this, &VueVille::btn_load_Click);
 			// 
 			// btn_insert
 			// 
-			this->btn_insert->Location = System::Drawing::Point(32, 353);
+			this->btn_insert->BackColor = System::Drawing::SystemColors::ActiveCaption;
+			this->btn_insert->Location = System::Drawing::Point(40, 441);
+			this->btn_insert->Margin = System::Windows::Forms::Padding(4);
 			this->btn_insert->Name = L"btn_insert";
-			this->btn_insert->Size = System::Drawing::Size(92, 47);
+			this->btn_insert->Size = System::Drawing::Size(115, 59);
 			this->btn_insert->TabIndex = 2;
 			this->btn_insert->Text = L"Nouveau";
-			this->btn_insert->UseVisualStyleBackColor = true;
+			this->btn_insert->UseVisualStyleBackColor = false;
 			this->btn_insert->Click += gcnew System::EventHandler(this, &VueVille::btn_insert_Click);
 			// 
 			// btn_delete
 			// 
-			this->btn_delete->Location = System::Drawing::Point(32, 546);
+			this->btn_delete->Location = System::Drawing::Point(40, 682);
+			this->btn_delete->Margin = System::Windows::Forms::Padding(4);
 			this->btn_delete->Name = L"btn_delete";
-			this->btn_delete->Size = System::Drawing::Size(92, 44);
+			this->btn_delete->Size = System::Drawing::Size(115, 55);
 			this->btn_delete->TabIndex = 3;
 			this->btn_delete->Text = L"Supprimer";
 			this->btn_delete->UseVisualStyleBackColor = true;
@@ -148,41 +157,47 @@ namespace ProjetProgrammationOrientéeObjetProgramme {
 			// 
 			// btn_update
 			// 
-			this->btn_update->Location = System::Drawing::Point(32, 457);
+			this->btn_update->BackColor = System::Drawing::SystemColors::ActiveCaption;
+			this->btn_update->Location = System::Drawing::Point(40, 571);
+			this->btn_update->Margin = System::Windows::Forms::Padding(4);
 			this->btn_update->Name = L"btn_update";
-			this->btn_update->Size = System::Drawing::Size(92, 49);
+			this->btn_update->Size = System::Drawing::Size(115, 61);
 			this->btn_update->TabIndex = 4;
 			this->btn_update->Text = L"Mise à jour";
-			this->btn_update->UseVisualStyleBackColor = true;
+			this->btn_update->UseVisualStyleBackColor = false;
 			this->btn_update->Click += gcnew System::EventHandler(this, &VueVille::btn_update_Click);
 			// 
 			// txt_id
 			// 
-			this->txt_id->Location = System::Drawing::Point(27, 144);
+			this->txt_id->Location = System::Drawing::Point(34, 180);
+			this->txt_id->Margin = System::Windows::Forms::Padding(4);
 			this->txt_id->Name = L"txt_id";
-			this->txt_id->Size = System::Drawing::Size(263, 20);
+			this->txt_id->Size = System::Drawing::Size(328, 22);
 			this->txt_id->TabIndex = 5;
 			// 
 			// txt_code
 			// 
-			this->txt_code->Location = System::Drawing::Point(27, 194);
+			this->txt_code->Location = System::Drawing::Point(34, 242);
+			this->txt_code->Margin = System::Windows::Forms::Padding(4);
 			this->txt_code->Name = L"txt_code";
-			this->txt_code->Size = System::Drawing::Size(263, 20);
+			this->txt_code->Size = System::Drawing::Size(328, 22);
 			this->txt_code->TabIndex = 6;
 			// 
 			// txt_nom
 			// 
-			this->txt_nom->Location = System::Drawing::Point(27, 244);
+			this->txt_nom->Location = System::Drawing::Point(34, 305);
+			this->txt_nom->Margin = System::Windows::Forms::Padding(4);
 			this->txt_nom->Name = L"txt_nom";
-			this->txt_nom->Size = System::Drawing::Size(264, 20);
+			this->txt_nom->Size = System::Drawing::Size(329, 22);
 			this->txt_nom->TabIndex = 7;
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(24, 128);
+			this->label1->Location = System::Drawing::Point(30, 160);
+			this->label1->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(26, 13);
+			this->label1->Size = System::Drawing::Size(33, 16);
 			this->label1->TabIndex = 8;
 			this->label1->Text = L"Ville";
 			this->label1->Click += gcnew System::EventHandler(this, &VueVille::label1_Click);
@@ -190,74 +205,86 @@ namespace ProjetProgrammationOrientéeObjetProgramme {
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(24, 228);
+			this->label2->Location = System::Drawing::Point(30, 285);
+			this->label2->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(30, 13);
+			this->label2->Size = System::Drawing::Size(38, 16);
 			this->label2->TabIndex = 9;
 			this->label2->Text = L"Pays";
 			// 
 			// richTextBox1
 			// 
-			this->richTextBox1->Location = System::Drawing::Point(556, 552);
+			this->richTextBox1->Location = System::Drawing::Point(695, 690);
+			this->richTextBox1->Margin = System::Windows::Forms::Padding(4);
 			this->richTextBox1->Name = L"richTextBox1";
-			this->richTextBox1->Size = System::Drawing::Size(286, 47);
+			this->richTextBox1->Size = System::Drawing::Size(356, 58);
 			this->richTextBox1->TabIndex = 14;
 			this->richTextBox1->Text = L"";
 			// 
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(500, 569);
+			this->label4->Location = System::Drawing::Point(625, 711);
+			this->label4->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(50, 13);
+			this->label4->Size = System::Drawing::Size(64, 16);
 			this->label4->TabIndex = 15;
 			this->label4->Text = L"Message";
 			// 
 			// btn_end
 			// 
-			this->btn_end->Location = System::Drawing::Point(273, 293);
+			this->btn_end->BackColor = System::Drawing::SystemColors::ButtonFace;
+			this->btn_end->Location = System::Drawing::Point(341, 366);
+			this->btn_end->Margin = System::Windows::Forms::Padding(4);
 			this->btn_end->Name = L"btn_end";
-			this->btn_end->Size = System::Drawing::Size(46, 23);
+			this->btn_end->Size = System::Drawing::Size(58, 29);
 			this->btn_end->TabIndex = 16;
 			this->btn_end->Text = L">>";
-			this->btn_end->UseVisualStyleBackColor = true;
+			this->btn_end->UseVisualStyleBackColor = false;
 			this->btn_end->Click += gcnew System::EventHandler(this, &VueVille::btn_end_Click);
 			// 
 			// btn_next
 			// 
-			this->btn_next->Location = System::Drawing::Point(190, 293);
+			this->btn_next->BackColor = System::Drawing::SystemColors::ButtonFace;
+			this->btn_next->Location = System::Drawing::Point(238, 366);
+			this->btn_next->Margin = System::Windows::Forms::Padding(4);
 			this->btn_next->Name = L"btn_next";
-			this->btn_next->Size = System::Drawing::Size(46, 23);
+			this->btn_next->Size = System::Drawing::Size(58, 29);
 			this->btn_next->TabIndex = 17;
 			this->btn_next->Text = L">";
-			this->btn_next->UseVisualStyleBackColor = true;
+			this->btn_next->UseVisualStyleBackColor = false;
 			this->btn_next->Click += gcnew System::EventHandler(this, &VueVille::btn_next_Click);
 			// 
 			// btn_previous
 			// 
-			this->btn_previous->Location = System::Drawing::Point(95, 293);
+			this->btn_previous->BackColor = System::Drawing::SystemColors::ButtonFace;
+			this->btn_previous->Location = System::Drawing::Point(119, 366);
+			this->btn_previous->Margin = System::Windows::Forms::Padding(4);
 			this->btn_previous->Name = L"btn_previous";
-			this->btn_previous->Size = System::Drawing::Size(46, 23);
+			this->btn_previous->Size = System::Drawing::Size(58, 29);
 			this->btn_previous->TabIndex = 18;
 			this->btn_previous->Text = L"<";
-			this->btn_previous->UseVisualStyleBackColor = true;
+			this->btn_previous->UseVisualStyleBackColor = false;
 			this->btn_previous->Click += gcnew System::EventHandler(this, &VueVille::btn_previous_Click);
 			// 
 			// btn_first
 			// 
-			this->btn_first->Location = System::Drawing::Point(12, 293);
+			this->btn_first->BackColor = System::Drawing::SystemColors::ButtonFace;
+			this->btn_first->Location = System::Drawing::Point(15, 366);
+			this->btn_first->Margin = System::Windows::Forms::Padding(4);
 			this->btn_first->Name = L"btn_first";
-			this->btn_first->Size = System::Drawing::Size(46, 23);
+			this->btn_first->Size = System::Drawing::Size(58, 29);
 			this->btn_first->TabIndex = 19;
 			this->btn_first->Text = L"<<";
-			this->btn_first->UseVisualStyleBackColor = true;
+			this->btn_first->UseVisualStyleBackColor = false;
 			this->btn_first->Click += gcnew System::EventHandler(this, &VueVille::btn_first_Click);
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(848, 552);
+			this->button2->Location = System::Drawing::Point(1060, 690);
+			this->button2->Margin = System::Windows::Forms::Padding(4);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(132, 47);
+			this->button2->Size = System::Drawing::Size(165, 59);
 			this->button2->TabIndex = 20;
 			this->button2->Text = L"Accès au menu";
 			this->button2->UseVisualStyleBackColor = true;
@@ -266,20 +293,35 @@ namespace ProjetProgrammationOrientéeObjetProgramme {
 			// label5
 			// 
 			this->label5->AutoSize = true;
-			this->label5->Location = System::Drawing::Point(24, 178);
+			this->label5->Location = System::Drawing::Point(30, 222);
+			this->label5->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(63, 13);
+			this->label5->Size = System::Drawing::Size(80, 16);
 			this->label5->TabIndex = 26;
 			this->label5->Text = L"Code postal";
 			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Gadugi", 31.8F, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->ForeColor = System::Drawing::Color::SlateGray;
+			this->label3->Location = System::Drawing::Point(531, 9);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(447, 65);
+			this->label3->TabIndex = 27;
+			this->label3->Text = L"Gestion des Villes";
+			this->label3->Click += gcnew System::EventHandler(this, &VueVille::label3_Click);
+			// 
 			// VueVille
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(96, 96);
+			this->AutoScaleDimensions = System::Drawing::SizeF(120, 120);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Dpi;
 			this->AutoSize = true;
 			this->BackColor = System::Drawing::SystemColors::ActiveCaption;
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
-			this->ClientSize = System::Drawing::Size(1049, 611);
+			this->ClientSize = System::Drawing::Size(1311, 764);
+			this->Controls->Add(this->label3);
 			this->Controls->Add(this->label5);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->btn_first);
@@ -298,6 +340,7 @@ namespace ProjetProgrammationOrientéeObjetProgramme {
 			this->Controls->Add(this->btn_insert);
 			this->Controls->Add(this->btn_load);
 			this->Controls->Add(this->dgv_enr);
+			this->Margin = System::Windows::Forms::Padding(4);
 			this->Name = L"VueVille";
 			this->Text = L"Golema";
 			this->Load += gcnew System::EventHandler(this, &VueVille::MyForm_Load);
@@ -383,5 +426,6 @@ namespace ProjetProgrammationOrientéeObjetProgramme {
 	private: System::Void button2_Click_1(System::Object^ sender, System::EventArgs^ e) {
 		this->Close();
 	}
-	};
+	
+};
 }
